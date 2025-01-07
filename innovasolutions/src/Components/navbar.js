c47449733bb0d2dd99806a0e57c12a49a6f7bb48
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-07 11:30:12 +0530

    add navbar to it

---
+++ innovasolutions/src/Components/navbar.js
@@ -0,0 +1,38 @@
+import '../index';
+
+export function Navbar() {
+    return (
+        <>
+            <navbar>
+                {/* company logo */}
+                <img src="" />
+                <ul>
+                    <li>
+                        Services
+                    </li>
+                    <li>
+                        Industries
+                    </li>
+                    <li>
+                        Partnerships
+                    </li>
+                    <li>
+                        Innova@Work
+                    </li>
+                    <li>
+                        Company
+                    </li>
+                    <li>
+                        Career
+                    </li>
+                    <li>
+                        Contact us
+                    </li>
+                </ul>
+                {/* search icon */}
+                <img src="" />
+            </navbar>
+
+        </>
+    )
+}
