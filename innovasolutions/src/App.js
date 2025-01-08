a3c6b496fbefa94a49d055bff8fa8798b98a4718
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-08 15:02:10 +0530

    body content one  finally done

--- innovasolutions/src/App.js
+++ innovasolutions/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './Components/navbar';
 import { Header } from './Components/Header';
+import { BodyOne } from './Components/BodyOne';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <BodyOne />
     </div>
   );
 }
---
+++ innovasolutions/src/Components/BodyOne.js
@@ -0,0 +1,12 @@
+export function BodyOne() {
+    return (
+        <>
+            <h1 style={{color: '#212169', fontWeight: '620', fontSize: '40px', marginTop: '50px'}}>
+                Where Innovation Comes to Life
+            </h1>
+            <p  className= 'bodyTextone'style={{}}>
+                Our vision is to be our clients’ most valuable technology services partner, an industry-thought leader, and generously give back to our communities.
+            </p>
+        </>
+    )
+}
