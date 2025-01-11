b4d3bb3525d43ac03a72ddf4bf6258e36b805ae7
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-11 13:01:12 +0530

    `Added styles and content to BodyFive component and updated index.css`

--- innovasolutions/src/Components/BodyFive.js
+++ innovasolutions/src/Components/BodyFive.js
@@ -1,19 +1,33 @@
-export function BodyFive(){
-    return(
+export function BodyFive() {
+    return (
         <>
-            <div className="container">
-                <h1>Services</h1>
+        <h1>Services</h1>
+            <div className="sercontainer">
+                
                 <div className="card">
-                    <div>
-                        <img src="https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp" />
+                    <img style={{ width: '40%' }} src="https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp" />
+
+                    <div className="card-body">
                         <h2>Cyber Security</h2>
                         <p>
-                        Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.
+                            Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.
+                        </p>
+                        <a href="" style={{border: 'none'}}>Learn More</a>
+                    </div>
+                </div>
+                <div className="card">
+                    <img style={{ width: '40%' }} src="https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp" />
+
+                    <div className="card-body">
+                        <h2>Cyber Security</h2>
+                        <p>
+                            Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.
                         </p>
                         <a href="">Learn More</a>
                     </div>
                 </div>
             </div>
+
         </>
     )
 }
