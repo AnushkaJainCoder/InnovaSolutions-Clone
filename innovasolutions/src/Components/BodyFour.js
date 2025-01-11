4d7daa59779deb371d52311d602f998eda2a94b9
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-11 12:38:57 +0530

    `Refactored BodyFour component and updated index.css for layout adjustments`

--- innovasolutions/src/Components/BodyFour.js
+++ innovasolutions/src/Components/BodyFour.js
@@ -1,35 +1,36 @@
 export function BodyFour(){
     return(
         <div className="bodyFour">
-            <h2>Our Digital-first Offerings</h2>
+            <h2 >Our Digital-first Offerings</h2>
             <div className="contenContainer">
-                <div>
-                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
-                    <p>Early-stage Opportunity Assessments</p>
-                </div>
-                <div>
-                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
-                    <p>Implementation of Data Programs</p>
-                </div>
-                <div>
-                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
-                    <p>Cloud
-                    Services</p>
-                </div>
-                <div>
+            <div className="cont">
                     <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
                     <p>AI/ML
                     Models</p>
                 </div>
-                <div>
+                <div className="cont">
                     <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
                     <p>Automated
                     Processes</p>
                 </div>
-                <div>
+                <div className="cont">
                     <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
                     <p>Customer Engagement Programs</p>
                 </div>
+                <div className="cont">
+                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
+                    <p>Early-stage Opportunity Assessments</p>
+                </div>
+                <div className="cont">
+                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
+                    <p>Implementation of Data Programs</p>
+                </div>
+                <div className="cont">
+                    <img src="https://www.innovasolutions.com/wp-content/uploads/2023/06/Early-stage-Opportunity-Assessments-Icon.svg" />
+                    <p>Cloud
+                    Services</p>
+                </div>
+                
             </div>
         </div>
     )
