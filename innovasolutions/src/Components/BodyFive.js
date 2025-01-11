3bfe4d61e8ab5bc54ac74e779a2674e1cc9e4622
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-11 14:47:50 +0530

    `Added state management and mapped card data to JSX components`

--- innovasolutions/src/Components/BodyFive.js
+++ innovasolutions/src/Components/BodyFive.js
@@ -1,31 +1,44 @@
+import { useState } from "react"
+
 export function BodyFive() {
+
+    const [activeCard, setActiveCard] = useState();
+    const cardData = [
+        {
+            title: 'Cyber Security',
+            desc: 'Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.',
+            imgsrc: 'https://www.innovasolutions.com/wp-content/uploads/2022/08/Talent-Solutions-copy.webp'
+        },
+        {
+            title: 'Cyber Security',
+            desc: 'Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.',
+            imgsrc: 'https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp'
+        },
+        {
+            title: 'Cyber Security',
+            desc: 'Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.',
+            imgsrc: 'https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp'
+        },
+        
+    ]
     return (
         <>
         <h1>Services</h1>
             <div className="sercontainer">
+                {cardData.map((card, index)=>(
+                    <div key={index} >
+                        <img src={card.imgsrc} alt={card.title}/>
+                        <div className="card-body">
+                            <h2>{card.title}</h2>
+                            <p >
+                                {card.description}
+                            </p>
+                            <a href="#" style={{ border: 'none' }}>Learn More</a>
+                        </div>
+                        
+                    </div>
+                ))}
                 
-                <div className="card">
-                    <img style={{ width: '40%' }} src="https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp" />
-
-                    <div className="card-body">
-                        <h2>Cyber Security</h2>
-                        <p>
-                            Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.
-                        </p>
-                        <a href="" style={{border: 'none'}}>Learn More</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <img style={{ width: '40%' }} src="https://www.innovasolutions.com/wp-content/uploads/2022/08/Cyber-Security-2.webp" />
-
-                    <div className="card-body">
-                        <h2>Cyber Security</h2>
-                        <p>
-                            Utilize the power of Innova’s cybersecurity offerings to deliver digital trust and neutralize threats effectively.
-                        </p>
-                        <a href="">Learn More</a>
-                    </div>
-                </div>
             </div>
 
         </>
