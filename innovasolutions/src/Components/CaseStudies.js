97711d05744ffdc6fd3d0cde92988620de0f9927
anushka.jain <anushka.jain@innovasolutions.com>
2025-01-22 18:01:12 +0530

    Added new case studies and technologies to CaseStudies.js and updated index.css for styling.

--- innovasolutions/src/Components/CaseStudies.js
+++ innovasolutions/src/Components/CaseStudies.js
@@ -10,19 +10,33 @@ export default function CaseStudies() {
     const cardData = [
         {
             imgSrc: 'https://www.innovasolutions.com/wp-content/uploads/2023/08/Change-Healthcare-cs-info.jpg',
-            title: 'Driving Digital Transformation and Streamlining Customer Data Management',
-            body: 'A global automotive leader, renowned for its innovative vehicles and dedication to customer satisfaction wanted to manage and integrate vast amounts of customer data with accuracy. Innova implemented a transactional-style Master Data Management (MDM) system to support the client’s global Digital Customer Experience Transformation (DCXT) program.'
+            title: 'Transforming Healthcare Data Management for Enhanced Efficiency and Insights',
+            body: 'A prominent global provider of healthcare software solutions and services faced significant challenges in data storage, data replication, and high operational costs. Our expert team designed and developed a cutting edge Data Lake Platform capable of handling and processing billions of transactions from various data types.'
+            ,technology: ' Informatica Power Center, IICS, Python, .NET, PL/SQL and Tableau'
+
         },
         {
             imgSrc: 'https://www.innovasolutions.com/wp-content/uploads/2023/08/Hyundai-Automotive-cs-info.jpg',
-            title: 'Driving Digital Transformation and Streamlining Customer Data Management',
-            body: 'A global automotive leader, renowned for its innovative vehicles and dedication to customer satisfaction wanted to manage and integrate vast amounts of customer data with accuracy. Innova implemented a transactional-style Master Data Management (MDM) system to support the client’s global Digital Customer Experience Transformation (DCXT) program.'
+            title: 'Revolutionizing Driving Experiences with an Integrated Platform Application',
+            body: 'A renowned multinational automotive manufacturer and a global leader in the automotive industry wanted to improve safety and security for drivers, streamline user experiences and enhance customer engagement. Innova developed a comprehensive platform to enhance operational efficiency that offered various car services with seamless communication, safety, and security.'
+            ,technology: '  Java with Springboot, Microservices'
         },
         {
             imgSrc: 'https://www.innovasolutions.com/wp-content/uploads/2023/08/Toyota-Industry-cs-info.jpg',
             title: 'Driving Digital Transformation and Streamlining Customer Data Management',
             body: 'A global automotive leader, renowned for its innovative vehicles and dedication to customer satisfaction wanted to manage and integrate vast amounts of customer data with accuracy. Innova implemented a transactional-style Master Data Management (MDM) system to support the client’s global Digital Customer Experience Transformation (DCXT) program.'
-        },
+            ,technology: '  Java with Springboot, Microservices' },
+        // {
+        //     imgSrc: 'https://www.innovasolutions.com/wp-content/uploads/2023/08/Toyota-Industry-cs-info.jpg',
+        //     title: 'Driving Digital Transformation and Streamlining Customer Data Management',
+        //     body: 'A global automotive leader, renowned for its innovative vehicles and dedication to customer satisfaction wanted to manage and integrate vast amounts of customer data with accuracy. Innova implemented a transactional-style Master Data Management (MDM) system to support the client’s global Digital Customer Experience Transformation (DCXT) program.'
+        // },
+        {
+            imgSrc: 'https://www.innovasolutions.com/wp-content/uploads/2023/09/Telecommunications-CaseStudy.jpg',
+            title: 'Enhancing Seamless Customer Experience Across Multiple Channels',
+            body: 'Discover how Innova Solutions revolutionized a leading telecommunications company’s customer experience across multiple channels, from retail to telesales, digital platforms to third-party retailers. By addressing unique business challenges and implementing innovative solutions, our client achieved remarkable results in order processing, sales growth, and operational excellence.'
+            ,technology: '  Java with Springboot, Microservices'},
+        
     ];
     const settings = {
         dots: true,
@@ -70,6 +84,11 @@ export default function CaseStudies() {
                                         <img src={card.imgSrc} className="caseStudies-img" alt={card.title} />
                                         <h4 style={{ marginTop: '11px' }}>{card.title}</h4>
                                         <p style={{ marginTop: '-11px' }}>{card.body}</p>
+                                        {/* <div>
+                                            <p></p>
+                                        </div> */}
+                                        <p style={{ marginTop: '-11px' }}><b>Technologies:</b> {card.technology}</p>
+
                                     </div>
                                 </div>
                             ))}
